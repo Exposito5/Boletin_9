ced1d78128f003df8803687312cfe31584ead004
Exposito5 <aecm.arturo@gmail.com>
2016-10-27 09:54:58 +0200

    EL serñor tiene mas pelo en las orejas que en el la cabeza

--- Boletin_5/Prueba B/src/prueba/b/PruebaB.java
+++ Boletin_5/Prueba B/src/prueba/b/PruebaB.java
@@ -4,5 +4,5 @@ public class PruebaB {
 
     public static void main(String[] args) {
 //6+4 la cara de tu retrato
-//El retrato de un señor mayor
+//Y si el señor mayor tiene mas pelo en las orejas que en la cabeza?
 }}

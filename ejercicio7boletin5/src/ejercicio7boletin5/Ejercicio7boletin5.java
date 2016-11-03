f8ea780addebbe5e6eec73ae001c7356b7bb156f
Exposito5 <aecm.arturo@gmail.com>
2016-11-03 09:05:42 +0100

    Sube

---
+++ ejercicio7boletin5/src/ejercicio7boletin5/Ejercicio7boletin5.java
@@ -0,0 +1,36 @@
+package ejercicio7boletin5;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brais
+ */
+public class Ejercicio7boletin5 {
+
+    public static void main(String[] args) {
+        
+        int select;
+        
+        Opciones objeto = new Opciones ();
+        
+     
+        select = Integer.parseInt(JOptionPane.showInputDialog("\n teclea 1 para circulo \n teclea 2 para cuadrado \n teclea 3 para triangulo"));
+        
+        switch (select){
+            
+            case 1 : objeto.circulo(0);
+            break;
+            case 2: objeto.cuadrado(0);
+            break;
+            case 3: objeto.triangulo(0, 0);
+            break;
+            
+            default:
+                
+            JOptionPane.showMessageDialog(null, "Opción escogida no válida, seleccione una opción de las 3 posibles, gracias.");
+                    
+        }
+        
+        }
+        }
---
+++ ejercicio7boletin5/src/ejercicio7boletin5/Opciones.java
@@ -0,0 +1,48 @@
+package ejercicio7boletin5;
+
+import javax.swing.JOptionPane;
+
+
+
+/**
+ *
+ * @author Brais
+ */
+public class Opciones {
+    private float lado,base,altura,radio,triangulo,cuadrado,circulo;
+    private int select;
+    private final float pi=3.14f;
+    
+    public Opciones(){
+        
+    }
+    
+    public Opciones(float lado,float base,float altura,float radio){
+        this.lado = lado;
+        this.base = base;
+        this.altura = altura;
+        this.radio = radio;
+       
+    }
+    
+    public void cuadrado(float lado){
+        lado = Float.parseFloat(JOptionPane.showInputDialog(" inserte el lado"));
+        JOptionPane.showMessageDialog(null,"el area del cuadrado es " +lado*lado);
+    }
+    
+    public void circulo (float radio){
+        radio = Float.parseFloat(JOptionPane.showInputDialog(" Inserte el radio "));
+        JOptionPane.showMessageDialog(null," el area de la circunferencia es "+ pi*Math.pow(radio,2));
+    }
+    
+    
+    public void triangulo(float base,float altura){
+        base = Float.parseFloat(JOptionPane.showInputDialog(" inserte la base "));
+        altura = Float.parseFloat(JOptionPane.showInputDialog(" Inserte la altura " ));
+        JOptionPane.showMessageDialog(null," el area del triangulo es "+ (base*altura)/2);
+        
+    }
+            
+            
+
+}
